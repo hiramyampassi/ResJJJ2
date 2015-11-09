702c4d92fc30bf58181ec1db0ddecb79e1f7b3a2
hiramyampassi <hiramyamen@gmail.com>
2015-11-09 01:24:36 -0400

    Modificacion ar

--- app/src/main/java/com/example/envydv6/resjjj/ar.java
+++ app/src/main/java/com/example/envydv6/resjjj/ar.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.envydv6.resjjj.R;
+
 
 /**
  * Fragmento para la sección de "Inicio"
